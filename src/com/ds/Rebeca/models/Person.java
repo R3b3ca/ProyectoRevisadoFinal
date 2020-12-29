66878e450664d253245513274f44c659b682013b
Rebeca Medina <rebeca.medina@ulead.ac.cr>
2020-12-28 21:34:16 -0600

    ProyectofinalProgra

---
+++ src/com/ds/Rebeca/models/Person.java
@@ -0,0 +1,33 @@
+package com.ds.Rebeca.models;
+
+public class Person {
+
+    protected String name;
+    protected String lastname;
+
+    public Person(final String name, final String lastname) {
+        super();
+        this.name = name.replace ( "\"", "");
+        this.lastname = lastname.replace (  "\"",  "");
+    }
+
+    public String toString() {
+        return "{\"name\":\""+ this.name + "\", \"lastname\":\"" + this.lastname + "\" }";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+}
